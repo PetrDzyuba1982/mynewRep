cf42cd2e427f9d3b4d0850dd604bc043935334cf
PetrDzyuba1982 <52494228+PetrDzyuba1982@users.noreply.github.com>
2022-02-22 09:11:27 +0200

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "Modul.h"
+using namespace std;
+int main(int argc, char* argv[])
+{ 
+	if (argc < 1) cout << "Vvedite imia fila"; else 
+	for(int i = 1;i<argc;i++)
+    cout << argv[i] << " ";
+	return 1000;
+}
